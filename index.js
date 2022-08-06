01169fc94bdd7f235c24e299c0cfd73155fc91e4
Kunal <33kunalg@gmail.com>
2022-08-06 08:47:50 +0530

    modified changes changes main

--- index.js
+++ index.js
@@ -66,3 +66,11 @@ async function weatherStation(keyword) {
       .then((output) => console.log(output.join("\n")))
     .catch((error) => console.error(error));
 
+    let a, b, c;  // Declare 3 variables
+    a = 5;        // Assign the value 5 to a
+    b = 6;        // Assign the value 6 to b
+    c = a + b;    // Assign the sum of a and b to c
+
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+;
