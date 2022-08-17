b8637e44ef54db542cd4a5953ceccb2718c8287e
Kunal <33kunalg@gmail.com>
2022-08-17 08:47:50 +0530

    modified topics js topics

--- index.js
+++ index.js
@@ -73,4 +73,7 @@ async function weatherStation(keyword) {
 
 const currencyEl_one = document.getElementById('currency-one');
 const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
 ;
